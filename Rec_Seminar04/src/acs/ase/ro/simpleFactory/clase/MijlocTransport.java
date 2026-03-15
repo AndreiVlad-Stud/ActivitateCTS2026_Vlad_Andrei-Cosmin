15dce3edfb2387cdab6df25ec9d2279c75c82e7d
Vlad Andrei Cosmin <vandrei.reclame@gmail.com>
2026-03-15 20:46:38 +0200

    Seminar 04

---
+++ Rec_Seminar04/src/acs/ase/ro/simpleFactory/clase/MijlocTransport.java
@@ -0,0 +1,13 @@
+package acs.ase.ro.simpleFactory.clase;
+
+public class MijlocTransport {
+    protected int numarRoti;
+    protected String numarInmatriculare;
+
+    public MijlocTransport(int numarRoti, String numarInmatriculare) {
+        this.numarRoti = numarRoti;
+        this.numarInmatriculare = numarInmatriculare;
+    }
+
+    public abstract void afiseazaDescriere();
+}
